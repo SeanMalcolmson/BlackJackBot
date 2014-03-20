f68669f4c2cd06cc12e0651b940f1262074eaa4d
SeanMalcolmson <sean_malcolmson@hotmail.com>
2014-03-20 13:23:21 -0400

    Added test match of two players. Plus fixes
    Added a test function test2Player() to test two computer players against
    one another using dealer strategy. also added who wins function to
    determine the winner of each round
    
    fixed the point value function that was miss deducting points for ace
    cards

--- BLACKJACKBOT.py
+++ BLACKJACKBOT.py
@@ -71,11 +71,18 @@ def point_value(hand):## Give it hand and it returns the point value of the hand
 		else: ## if there is an ace first assume to give the player the higher value (11)
 			points +=11
 	if points > 21 and has_ace(hand): ## but if by taking the higher value of the ace(11) it would cause a bust value ace at 1 
-		points -= 10
+		aceCount = 0
+		for cards in hand: ## added this to account for hands with multiple aces 
+			if cards[0] == 'Ace':
+				aceCount+=1
+		while points > 21 and aceCount>=1:
+			aceCount -= 1
+			points -= 10
 	return points
 
 
-
+"""
+## NOTE maybe switch all fuctions to take player as the argument instead of player.hand to make things cleaner 
 newDeck = Deck()
 print newDeck.cards
 newDeck.shuffle()
@@ -95,16 +102,50 @@ print "Do they have an ace? :" + str(has_ace(player1.hand))
 print "Did they bust? :" + str( bust(player1) )
 
 """
-shuffleDeck(Deck_of_cards)
-player1 = Player()
-player1.hand = deal(Deck_of_cards,2)
-print player1.hand
-print point_value(player1.hand)
 
-hit(player1,Deck_of_cards)
+def who_wins(dealer,player):
+	if bust(player):
+		player.money -=5
+		return dealer.name
+	elif bust(dealer):
+		player.money += 5
+		return player.name
+	elif point_value(dealer.hand) > point_value(player.hand):
+		player.money -=5
+		return dealer.name
+	elif point_value(dealer.hand) == point_value(player.hand):
+		return "Tie Game"
+	else:
+		player.money +=5
+		return player.name
 
-print player1.hand
-print point_value(player1.hand)
+
+### working on below 
+def test2Player(): ## Player vs Dealer both using dealer stratagy 
+	dealer =  Player()
+	dealer.name = 'Dealer'
+	# assuming the dealer has unlimted money
+	player2 = Player()
+	player2.name = 'Player2'
+	player2.money = 100
+	freshDeck  = Deck()
+	freshDeck.shuffle()
+	while len(freshDeck.cards) > 8:
+		dealer.hand = deal(freshDeck.cards,2)
+		player2.hand = deal(freshDeck.cards,2)
+		print "Dealer start hand:" + str(dealer.hand)
+		print "player2 start hand:" + str(player2.hand)
+		while point_value(dealer.hand) < 17:
+			hit(dealer,freshDeck.cards)
+		while point_value(player2.hand) < 17:
+			hit(player2,freshDeck.cards)
+		print "Dealer end hand:" + str(dealer.hand) + "its point value is:" + str(point_value(dealer.hand))
+		print "player2 end hand:" + str(player2.hand) + "its point value is:" + str(point_value(player2.hand))
+
+		print "the winner is: " +who_wins(dealer,player2)
+	print "player 2 has this much money: " + str(player2.money)
 
 
-"""
+
+
+test2Player()
